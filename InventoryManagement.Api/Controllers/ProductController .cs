28910e46488c8f4f215b95b6eb8e805f3fa31949
Reyning <perdomoreyning@gmail.com>
2024-08-05 22:08:37 -0400

    jewelry

---
+++ InventoryManagement.Api/Controllers/ProductController .cs
@@ -0,0 +1,36 @@
+using Employeees.Domain.Entities;
+using Employees.Web.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(Name = "GetProducts")]
+        public IEnumerable<Product> Get()
+        {
+            var productsFromDb = _context.Products.ToList();
+            return productsFromDb;
+        }
+
+        [HttpGet("{id}", Name = "GetProducts")]
+        public ActionResult<Product> Get(int id)
+        {
+            var productsFromDb = _context.Employees.FirstOrDefault(p => p.Id == id);
+            if (productsFromDb == null)
+            {
+                return NotFound("Product not found");
+            }
+        }
+    }
+}
