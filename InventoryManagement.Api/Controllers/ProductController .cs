7f5c0e044df6dd2068c1e2a02233d500987796bf
Reyning <perdomoreyning@gmail.com>
2024-08-06 11:28:27 -0400

    make work the web project after the segregation of entity and separate the entity request of the  controller only on the  create/post

--- InventoryManagement.Api/Controllers/ProductController .cs
+++ InventoryManagement.Api/Controllers/ProductController .cs
@@ -1,7 +1,6 @@
 using Employeees.Domain.Entities;
 using Employees.Web.Persistence;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace InventoryManagement.Api.Controllers
 {
